bbf60383edfb17b9430b23a3b0fe9786db0350ee
wudizhangzhi <yueyatianchong@qq.com>
2021-06-16 22:45:47 +0800

    update

--- constant.go
+++ constant.go
@@ -3,4 +3,11 @@ package HupuApp
 const (
 	DefaultLiveWebSocketDomain = "61.174.11.224:3081"
 	DefaultTac                 = "tac.csv"
+	API_VERSION                = "7.1.15"
+	HUPU_SALT                  = ""
+	// 接口
+	// 直播接口
+	API_INIT             = "https://games.mobileapi.hupu.com/1/" + API_VERSION + "/status/init"
+	API_GET_GAMES        = "https://games.mobileapi.hupu.com/1/" + API_VERSION + "/%s/getGames"
+	API_GET_PLAY_BY_PLAY = "https://games.mobileapi.hupu.com/1/" + API_VERSION + "/room/getPlaybyplay"
 )
---
+++ gohupu/api/base.go
@@ -0,0 +1,82 @@
+package api
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"net/http"
+	"sort"
+	"strings"
+
+	"github.com/wudizhangzhi/HupuApp"
+)
+
+var HupuHttpobj HupuHttp
+
+type HupuHttp struct {
+	Client  *http.Client
+	Method  string
+	Params  map[string]string
+	Headers map[string]string
+}
+
+// def getSortParam(**kwargs):
+//     result = ''
+//     kwargs_sorted = sorted(kwargs)
+//     for key in kwargs_sorted:
+//         if len(result) > 0:
+//             result += '&'
+//         result += '='.join((key, str(kwargs.get(key))))
+//     result += HUPU_SALT
+//     return md5(result.encode('utf8')).hexdigest()
+
+func init() {
+	HupuHttpobj = HupuHttp{
+		Client: &http.Client{},
+	}
+}
+
+func getSortParam(params map[string]string) string {
+	result := ""
+	keys := make([]string, 0)
+	for k, _ := range params {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	for _, key := range keys {
+		if len(result) > 0 {
+			result += "&"
+		}
+		result += strings.Join([]string{key, params[key]}, "=")
+	}
+	result += HupuApp.HUPU_SALT
+	h := md5.New()
+	h.Write([]byte(result))
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+func (hh *HupuHttp) Request(method string, url string, headers map[string]string, params map[string]string) (*http.Response, error) {
+	sign := getSortParam(params)
+	params["sign"] = sign
+	switch method {
+	case "POST":
+		data := make(map[string][]string)
+		for k, v := range params {
+			data[k] = []string{v}
+		}
+		return hh.Client.PostForm(url, data)
+	case "GET":
+		// format url param
+		req, err := http.NewRequest(method, url, nil)
+		if err != nil {
+			return nil, err
+		}
+		q := req.URL.Query()
+		for k, v := range params {
+			q.Add(k, v)
+		}
+		req.URL.RawQuery = q.Encode()
+		return hh.Client.Do(req)
+	}
+
+	return nil, nil
+}
